149c4ceae156f7f516f3df444f73fba7835da9af
Kevin <Kevin@DESKTOP-BJMMBGU>
2022-10-25 14:26:31 +0800

    publicpath調整

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
   // vue.config.js 設定指南 : https://cli.vuejs.org/zh/config/#%E5%85%A8%E5%B1%80-cli-%E9%85%8D%E7%BD%AE
   module.exports = {
       // publicPath 為 project-name，根目錄地址為上傳的網域
-      publicPath: '/my_Resume/',
+      publicPath: '/myResume/',
   };  
